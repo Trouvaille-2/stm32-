dae61e0ecd519b917e2e7600aa66d7e073ff26d4
Trouvaille-2 <2015452145@qq.com>
2026-05-04 22:04:06 +0800

    5.4

---
+++ ESP32/hello_world/main/hello_world_main.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+void app_main(void)
+{
+    while(1)
+    {
+        printf("Hello world!\n");
+        vTaskDelay(500);
+    }
+}
